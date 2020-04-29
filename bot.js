06ceec01fe2daee36a95516e7152224a8df7f75b
Matthew Murphy <mdmurphy2@wisc.edu>
2020-04-29 17:02:55 -0500

    Added functionality for tracking a presence change. TODO: Connect to database

--- bot.js
+++ bot.js
@@ -22,12 +22,62 @@ client.on('message', msg => {
 });
 
 
-client.on('presenceUpdate', (oldMember, newMember) => {
-  console.log('------------PRESENCEUPDATE-------------');
-  //console.log(oldMember);
-  console.log('------------PRESENCEUPDATE-------------');
+//ServerID newMember.guild.id
+//UserID newmember.userID
+//applicationID newmember.activities[i].applicationID
+//applicationName newmember.activities[i].name
 
+//username of user by ID newMember.guild.members.fetch(newMember.userID).then(user => console.log(user.user.username));
+//nickname of user by ID newMember.guild.members.fetch(newMember.userID).then(user => console.log(user.nickname));
+
+//Check if start time has changed or if that activity is gone
+client.on('presenceUpdate', (oldMember, newMember) => {
+  let oldActivities = oldMember.activities;
+  let newActivities = newMember.activities;
+  let oldActivity;
+  let newActivity;
+  let foundMatch;
+  let msPlayed = 0;
+
+
+
+
+  //Check new activities vs old activities 
+  for(let i = 0; i < oldActivities.length; i++) {
+    oldActivity = oldActivities[i];
+    foundMatch = false;
+    for(let j = 0; j < newActivities.length; j++) {
+      newActivity = newActivities[j];
+      let oldActivityId  = oldActivity.applicationID ? oldActivity.applicationID : oldActivity.name;
+      let newActivityId  = newActivity.applicationID ? newActivity.applicationID : newActivity.name;
+      if(oldActivityId == newActivityId) {
+        if(oldActivity.createdTimestamp != newActivity.createdTimestamp) { //Check if start time is different
+          //Update Time played here with NEW - OLD
+          msPlayed = newActivity.createdTimestamp - oldActivity.createdTimestamp;
+          console.log('Not even timestamps');
+          console.log("You played " + oldActivity.name + " for " + msPlayed);
+          msPlayed = 0;
+          
+        }
+        foundMatch = true;
+        break;
+      }
+    }
+    if(!foundMatch) { //The game is no longer being played
+      //Update time played here with currenttime - OLD
+      //let startTime = new Date(oldActivity.createdTimestamp).getTime();
+      let endTime = Date.now();
+      msPlayed = endTime - oldActivity.createdTimestamp;
+      console.log('ended activity');
+      console.log("You played " + oldActivity.name + " for " + msPlayed);
+      msPlayed = 0;
+    }
+  }
 });
+
+
+
+
 
 
 client.login(auth.token);
@@ -62,12 +112,5 @@ function replyCurrentGame(msg) {
   } else {
     message = " You are not currently playing a game.";
   }
-
   msg.reply(message);
- 
-  console.log('------------ACTIVITIES-------------');
-  console.log(currentGame);
-  console.log('------------ACTIVITIES-------------');
-  //let message = ", You have been playing " + currentGame + " for " + time " "
-  //msg.reply()
 }
